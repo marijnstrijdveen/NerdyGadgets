93413e9e45b0566fe8dfc2494c5c57980f62a15f
Joeyboy1003 <joeypennings@gmail.com>
2020-11-12 16:34:45 +0100

    Eerste opzet contactpagina

--- nerdygadgets/contact.php
+++ nerdygadgets/contact.php
@@ -1,1 +1,12 @@
-hoi
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>contactpagina</title>
+</head>
+<body>
+Welkom op de contactpagina van NerdyGadgets! <br> <br>
+Email: contact@nerdygadgets.nl <br>
+Tel: 030 - 123 45 67 <br>
+</body>
+</html>
