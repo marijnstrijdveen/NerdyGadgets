d0c2feefab7b3b748373f50623c210d4a2e9e7c9
Joeyboy1003 <joeypennings@gmail.com>
2020-11-16 11:00:56 +0100

    Tweede opzet contactpagina

--- nerdygadgets/contact.php
+++ nerdygadgets/contact.php
@@ -12,6 +12,8 @@ include "header.php";
 Welkom op de contactpagina van NerdyGadgets! <br> <br>
 Email: contact@nerdygadgets.nl <br>
 Tel: 030 - 123 45 67 <br>
+Adres: Campus 2, 8017 CA Zwolle <br>
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2428.874717150838!2d6.077876615793108!3d52.49950737981033!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c7ded4e818a589%3A0x68b47bb437213d3b!2sHogeschool%20Windesheim!5e0!3m2!1snl!2snl!4v1605520791199!5m2!1snl!2snl" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
 </body>
 </html>
 
@@ -20,8 +22,10 @@ include "footer.php";
 ?>
 
 <?php
-/* Nog toe te voegen (aan header?)
+/* Is toegvoegd aan header (line 76)
 ?>
 <li>
     <a href="contact.php" class="HrefDecoration">Contactpagina</a>
 </li>
+*/
+?>
