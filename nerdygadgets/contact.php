fa2d98b4885430b05236589a81758c161a16f556
Joeyboy1003 <joeypennings@gmail.com>
2020-11-16 11:31:24 +0100

    Tweede opzet contactpagina

--- nerdygadgets/contact.php
+++ nerdygadgets/contact.php
@@ -10,10 +10,21 @@ include "header.php";
 </head>
 <body>
 Welkom op de contactpagina van NerdyGadgets! <br> <br>
+Over ons: <br>
+“Wij zijn Nerdygadgets. We zijn trots op onze producten en geloven in kwaliteit. In ieder huis vindt een product van Nerdygadgets zijn thuis.<br>
+Ondanks onze grootte zien klanten ons als kleinschalig en betrokken. Persoonlijk klantcontact staat bij ons hoog in het vaandel.” <br> <br>
 Email: contact@nerdygadgets.nl <br>
-Tel: 030 - 123 45 67 <br>
-Adres: Campus 2, 8017 CA Zwolle <br>
-<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2428.874717150838!2d6.077876615793108!3d52.49950737981033!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c7ded4e818a589%3A0x68b47bb437213d3b!2sHogeschool%20Windesheim!5e0!3m2!1snl!2snl!4v1605520791199!5m2!1snl!2snl" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+Tel: 030 - 123 45 67 <br> <br>
+Openingstijden: <br>
+Maandag 08:00 - 21:00 <br>
+Dinsdag 08:00 - 21:00 <br>
+Woensdag 08:00 - 21:00 <br>
+Donderdag 08:00 - 21:00 <br>
+Vrijdag 08:00 - 21:00 <br>
+Zaterdag 08:00 - 21:00 <br>
+Zondag 09:00 - 19:00 <br> <br>
+Adres: Stationsplein, 1012 AB Amsterdam <br>
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d78016.33903673291!2d4.835525476262086!3d52.33395389313914!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c609b70dd81623%3A0xcae71b8d3adfd142!2sAmsterdam%20Centraal!5e0!3m2!1snl!2snl!4v1605522129904!5m2!1snl!2snl" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe> <br>
 </body>
 </html>
 
