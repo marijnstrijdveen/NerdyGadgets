0fc83dd813b51e166b5f90a43db4cdb2c0ad8053
Joeyboy1003 <joeypennings@gmail.com>
2020-11-26 15:46:12 +0100

    Contactpagina aanpassen

--- nerdygadgets/contact.php
+++ nerdygadgets/contact.php
@@ -1,5 +1,22 @@
 <?php
 include "header.php";
+
+$query= "SELECT *
+FROM contact";
+
+$Statement = mysqli_query($Connection, $query);
+$R = mysqli_fetch_all($Statement, MYSQLI_ASSOC);
+
+$email = "";
+$tel = "";
+$adres = "";
+
+foreach ($R as $info){
+    $email = $info['email'];
+    $tel = $info['phone'];
+    $adres = $info['address'];
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -26,8 +43,9 @@ Vrijdag 08:00 - 21:00 <br>
 Zaterdag 08:00 - 21:00 <br>
 Zondag 09:00 - 19:00 <br> <br>
 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d78016.33903673291!2d4.835525476262086!3d52.33395389313914!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c609b70dd81623%3A0xcae71b8d3adfd142!2sAmsterdam%20Centraal!5e0!3m2!1snl!2snl!4v1605522129904!5m2!1snl!2snl" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe> <br>
+    <a href="contactAdministratie.php" class="HrefDecoration">Pagina bijwerken</a>
 </div>
-<a href="contactAdministratie.php" class="HrefDecoration">Pagina bijwerken</a>
+
 
 </body>
 </html>
