061f643e2082a96bb40a05211dc58f29e4caad35
Yana <y.abbink28@gmail.com>
2020-12-06 17:18:48 +0100

    Add to verlijnlijstje knopjes Delete and knop Add te cart.

--- nerdygadgets/verlanglijstje.php
+++ nerdygadgets/verlanglijstje.php
@@ -1,6 +1,32 @@
 <?php
 include __DIR__ . '/init.php';
 /** @var $Connection mysqli */
+/** @var $conn mysqli */
+
+if (isset($_GET['add'])) {
+    $shows = new shows();
+    $info = $shows->add_cart();
+}
+
+if (isset($_GET['wish'])) {
+    // Remove record from table "wishlist" by data:
+    //  $_SESSION['userid']; // Account ID
+    // and
+    // $_GET['wish']; // Stock Item ID
+
+    // DELETE FROM table_name WHERE officeCode = 4 AND ...;
+    $deleteToWishlistSql = "DELETE FROM `wishlist` WHERE account_id = ? AND stock_item_id = ?;";
+
+    try {
+        $Statement = mysqli_prepare($Connection, $deleteToWishlistSql);
+        mysqli_stmt_bind_param($Statement, 'ii', $_SESSION['userid'], $_GET['wish']);
+        mysqli_stmt_execute($Statement);
+
+        $info = '<div class="alert alert-success">Product deleted from wishlist.</div>';
+    } catch (Throwable $exception) {
+        $info = '<div class="alert alert-danger">Is not present in your wishlist.</div>';
+    }
+}
 
 $currentUserId = $_SESSION['userid'];
 $ShowStockLevel = 1000;
@@ -29,6 +55,7 @@ include __DIR__ . '/header.php';
 ?>
 
 <div id="ResultsArea" class="Browse">
+    <?=$info ?? ''?>
     <h2>Your wishlist</h2>
     <?php
     if (isset($ReturnableResult) && count($ReturnableResult) > 0):
@@ -48,10 +75,12 @@ include __DIR__ . '/header.php';
                     <div class="col-6">
                         <a href='view.php?id=<?=$row['StockItemID']?>'>
                             <h1 class="StockItemID">Product number <?=$row["StockItemID"]?></h1>
-                            <div class="StockItemName"><?=$row["StockItemName"]?></div>
-                            <div class="StockItemComments"><?=$row["MarketingComments"]?></div>
+                            <div class="StockItemName"><?=$row['StockItemName']?></div>
+                            <div class="StockItemComments"><?=$row['MarketingComments']?></div>
                         </a>
-                        <div class="StockItemButtons"><button>BEEP</button></div>
+                        <a href="verlanglijstje.php?add=<?=$row['StockItemID']?>" class="cart-btn">Add to Cart</a>
+
+                        <a href="verlanglijstje.php?wish=<?=$row['StockItemID']?>" class="btn btn-primary">Remove from wishlist</a>
                     </div>
                     <div class="col-2">
                         <h1 class="StockItemPriceText"><?php print sprintf("€ %0.2f", $row["SellPrice"]); ?></h1>
