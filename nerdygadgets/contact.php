5fd115faaf47a6de830bb273937e37889fca3707
pauli <paulienstrijbos@live.nl>
2020-12-08 17:30:35 +0100

    :)

--- nerdygadgets/contact.php
+++ nerdygadgets/contact.php
@@ -31,13 +31,13 @@ Despite the size of our company, many customers will say we appear small-scale a
 <b>Phone number:</b> <?php echo $tel?> <br>
 <b>Address:</b> <?php echo $adres?> <br> <br>
 <b>Business hours:</b> <br>
-Monday 08:00 - 21:00 <br>
-Tuesday 08:00 - 21:00 <br>
-Wednesday 08:00 - 21:00 <br>
-Thursday 08:00 - 21:00 <br>
-Friday 08:00 - 21:00 <br>
-Saturday 08:00 - 21:00 <br>
-Sunday 09:00 - 19:00 <br> <br>
+Monday 08:00 AM - 21:00 PM <br>
+Tuesday 08:00 AM - 21:00 PM <br>
+Wednesday 08:00 AM - 21:00 PM <br>
+Thursday 08:00 AM - 21:00 PM <br>
+Friday 08:00 AM - 21:00 PM <br>
+Saturday 08:00 AM - 21:00 PM <br>
+Sunday 09:00 AM - 19:00 PM <br> <br>
 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d78016.33903673291!2d4.835525476262086!3d52.33395389313914!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c609b70dd81623%3A0xcae71b8d3adfd142!2sAmsterdam%20Centraal!5e0!3m2!1snl!2snl!4v1605522129904!5m2!1snl!2snl" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe> <br>
 
 <?php
